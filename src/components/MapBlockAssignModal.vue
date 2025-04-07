7784afbdb199c321bdbf64d4c74c73155fefc85f
Lorenz Becislao <lorenz.becislao@lafilgroup.com>
2025-04-07 18:04:11 +0800

    Add implementation for bin transfer

--- src/components/MapBlockAssignModal.vue
+++ src/components/MapBlockAssignModal.vue
@@ -22,7 +22,7 @@ const selectedLayerIndex = ref(-1);
 const selectedLayer = ref(null);
 const searchValue = ref('');
 const selectedInventory = ref(null);
-const selectedNewInventory = ref(null); // For Bin Transfer
+const selectedNewBlock = ref(null); // For Bin Transfer
 const serverItems = ref([]);
 const loading = ref(true);
 const totalItems = ref(0);
@@ -65,6 +65,26 @@ const headers = [
     },
 ]
 
+const layerHeaders = [
+    {
+        title: 'BLOCK',
+        key: 'block',
+        sortable: false
+    },
+    {
+        title: 'ASSIGNED ITEM',
+        key: 'assigned_item',
+        align: 'center',
+        sortable: false
+    },
+    {
+        title: '',
+        key: 'action',
+        sortable: false,
+        align: 'end',
+    },
+]
+
 onMounted(() => {
     loadItems({
         page: page.value,
@@ -85,7 +105,7 @@ const loadItems = ({ page, itemsPerPage, sortBy, search }) => {
     } else {
         sortQuery.value = '-updated_at';
     }
-
+    
     ApiService.query(`warehouse/get-grouped-inventories/${props.storageLocation}`,{
         params: {
             page,
@@ -115,6 +135,51 @@ const loadItems = ({ page, itemsPerPage, sortBy, search }) => {
         });
 }
 
+const layerItems = ref([]);
+const layersLoading = ref(true);
+const totalLayerItems = ref(0);
+const layersItemsPerPage = ref(5);
+const layerPage = ref(1);
+const layerSortQuery = ref('-updated_at');
+const layerSearchValue = ref('');
+
+const loadAvailableBlocks = ({ page, itemsPerPage, sortBy, layerSearch }) => {
+    layersLoading.value = true
+    if (sortBy && sortBy.length > 0) {
+        const sort = sortBy[0];  // Assuming single sort field
+        sortQuery.value = `${sort.key}`;  // Default ascending order
+        if (sort.order === 'desc') {
+            sortQuery.value = `-${sort.key}`;  // Prefix with minus for descending order
+        }
+    } else {
+        sortQuery.value = '-blocks.updated_at';
+    }
+    
+    ApiService.query(`warehouse/get-available-blocks/${props.storageLocation}`,{
+        params: {
+            page,
+            itemsPerPage,
+            sort: layerSortQuery.value,
+            search: layerSearchValue.value
+        }
+        })
+        .then((response) => {
+            totalLayerItems.value = response.data.total;
+            layerItems.value = response.data.data.map(item => ({
+                id: item.id,
+                label: item.label,
+                inventories_count: item.inventories.length || 0,
+                isSelected: false,
+                lot: item.lot || null
+            }));
+
+            layersLoading.value = false
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
 const toast = ref({
     message: '',
     color: 'success',
@@ -126,6 +191,7 @@ const closeModal = () => {
     selectedLayerIndex.value = -1;
     selectedLayer.value = null;
     enableBinTransfer.value = false;
+    selectedNewBlock.value = false
 }
 
 const selectLayer = (index, layer) => {
@@ -175,10 +241,11 @@ const items = [
     { title: 'For RTM' },
 ]
 
-const handleActionClick = (action, layer) => {
+const handleActionClick = (action, layer, index) => {
     selectedAction.type = action;
     selectedInventory.value = layer.assigned_inventory;
-    console.log(layer);
+    selectedLayerIndex.value = index;
+    selectedLayer.value = layer;
     
     if (action === 'Bin Transfer') {
         enableBinTransfer.value = true;
@@ -215,19 +282,22 @@ const proceedAction = async () => {
         } else {
             const response = await axios.post('warehouse/bin-transfer', {
                 block: props.block.data,
-                inventory: selectedInventory.value
+                inventory: selectedInventory.value,
+                newBlock: selectedNewBlock.value,
+                layer: selectedLayer.value
             });
 
             if (response.status === 200) {
-                loadItems({
-                    page: page.value,
-                    itemsPerPage: itemsPerPage.value,
-                    sortBy: [{key: 'updated_at', order: 'desc'}],
-                    search: searchValue.value
+                loadAvailableBlocks({
+                    page: layerPage.value,
+                    itemsPerPage: layersItemsPerPage.value,
+                    sortBy: [{key: '-blocks.updated_at', order: 'desc'}],
+                    search: layerSearchValue.value
                 });
                 selectedLayerIndex.value = -1;
                 selectedLayer.value = null;
                 selectedInventory.value = null;
+                selectedNewBlock.value = null;
             }
         }
         actionModalOpen.value = false;
@@ -242,9 +312,9 @@ const proceedAction = async () => {
 };
 
 const binTransfer = (item) => {
-    selectedNewInventory.value = item;
+    selectedNewBlock.value = item;
     selectedAction.title = 'Bin Transfer';
-    selectedAction.message = `Are you sure you want to transfer RFID with physical ID of <strong>${selectedNewInventory.value.rfid?.name}</strong> to the bin of RFID with physical ID of <strong>${selectedInventory.value.rfid?.name}</strong>?`;
+    selectedAction.message = `Transfer selected RFID with physical ID of <strong>${selectedInventory.value.rfid?.name}</strong> to bin location <strong>${selectedNewBlock.value.lot?.label} - ${selectedNewBlock.value.label}</strong>?`;
     actionModalOpen.value = true;
 }
 
@@ -267,7 +337,8 @@ const handleSearch = debounce((search) => {
                 >
                     <VListItem class="py-0 px-0" :class="selectedLayerIndex === index ? 'bg-primary-light' : 'bg-transparent'">
                         <template v-if="layer.assigned_inventory">
-                            <VListItem :class="layer.layer_class"> 
+                            <VListItem :class="[layer.layer_class, 
+                                (selectedLayerIndex === index && enableBinTransfer) ? 'highlighted-item' : '']">
                                 <VListItemTitle>
                                     <span class="text-h5 font-weight-bold text-white" >{{ layer.layer_name }}</span>
                                 </VListItemTitle>
@@ -289,7 +360,7 @@ const handleSearch = debounce((search) => {
                                                 v-for="(item, i) in items"
                                                     :key="i"
                                                     :value="i"
-                                                    @click="handleActionClick(item.title, layer)"
+                                                    @click="handleActionClick(item.title, layer, index)"
                                                 >
                                                 <v-list-item-title>{{ item.title }}</v-list-item-title>
                                             </v-list-item>
@@ -346,7 +417,43 @@ const handleSearch = debounce((search) => {
                 Cancel Bin Transfer
             </v-btn>
         </div>
-        <VDataTableServer
+
+        <VDataTableServer v-if="enableBinTransfer"
+            v-model:items-per-page="layersItemsPerPage"
+            :headers="layerHeaders"
+            :items="layerItems"
+            :items-length="totalLayerItems"
+            :loading="layersLoading"
+            item-value="id"
+            :search="layerSearchValue"
+            @update:options="loadAvailableBlocks"
+            class="text-no-wrap"
+        >
+
+            <template v-slot:item="{ item }">
+                <tr class="text-no-wrap">
+                    <td style="width: 200px;">{{ item.lot?.label }} - {{ item.label }}</td>
+                    <td style="width: 200px;" class="text-center">{{ item.inventories_count }}</td>
+                    <td style="width: 200px;">
+                        <div v-if="enableBinTransfer" class="d-flex justify-end align-center">
+                            <v-btn @click="binTransfer(item)" class="px-5" type="button" color="info">
+                                Bin Transfer
+                            </v-btn>
+                        </div> 
+                         <div v-else class="d-flex justify-end align-center">
+                            <v-btn :disabled="true" v-if="item.isAssigned" class="px-5" type="button" style="background-color: #eece70 !important;">
+                                Assigned
+                            </v-btn>
+                            <v-btn v-else :disabled="selectedLayer == null" @click="assign(item)" class="px-5" type="button" color="primary-light">
+                                Assign
+                            </v-btn>
+                        </div> 
+                    </td>
+                </tr>
+            </template> 
+        </VDataTableServer>
+
+        <VDataTableServer v-else
             v-model:items-per-page="itemsPerPage"
             :headers="headers"
             :items="serverItems"
@@ -380,8 +487,8 @@ const handleSearch = debounce((search) => {
                     </td>
                 </tr>
             </template>
-
         </VDataTableServer>
+
         <div class="d-flex justify-end align-center mt-4">
             <v-btn color="secondary" variant="outlined" @click="closeModal" class="px-12 mr-3">Close</v-btn>
         </div>
@@ -447,6 +554,10 @@ const handleSearch = debounce((search) => {
 }
 .empty-layer, .layer-0 {
     background-color: #f0edf2;
+}
+
+.highlighted-item {
+    border: 3px solid #00833c;  /* Green border to indicate selection */
 }
 
 </style>
