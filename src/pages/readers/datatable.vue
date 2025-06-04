96337e59c9994b6be89692b4333914046a0c746a
Lorenz Becislao <lorenz.becislao@lafilgroup.com>
2025-06-04 11:29:48 +0800

    Fix last log attribute to use created_at instead of first seen timestamp on reader

--- src/pages/readers/datatable.vue
+++ src/pages/readers/datatable.vue
@@ -81,7 +81,7 @@ const headers = [
 ]
 
 const loadItems = ({ page, itemsPerPage, sortBy, search }) => {
-    
+
     loading.value = true
     if (sortBy && sortBy.length > 0) {
         const sort = sortBy[0];  // Assuming single sort field
@@ -93,7 +93,7 @@ const loadItems = ({ page, itemsPerPage, sortBy, search }) => {
         sortQuery.value = '-created_at';
     }
 
-    ApiService.query('datatable/readers',{
+    ApiService.query('datatable/readers', {
         params: {
             page,
             itemsPerPage,
@@ -101,7 +101,7 @@ const loadItems = ({ page, itemsPerPage, sortBy, search }) => {
             search: props.search,
             filters: filters.value
         }
-        })
+    })
         .then((response) => {
             totalItems.value = response.data.total;
             serverItems.value = response.data.data
@@ -127,7 +127,7 @@ const editItem = (item) => {
     form.value.plant_code = item.plant?.plant_code
     form.value.ip_address = item.ip_address
     editDialog.value = true;
-}  
+}
 
 const deleteItem = (item) => {
     selectedReader.value = item;
@@ -144,7 +144,7 @@ const handleDelete = async () => {
         loadItems({
             page: page.value,
             itemsPerPage: itemsPerPage.value,
-            sortBy: [{key: 'created_at', order: 'desc'}],
+            sortBy: [{ key: 'created_at', order: 'desc' }],
             search: props.search
         });
         toast.value.message = 'Reader deleted successfully!'
@@ -164,7 +164,7 @@ const handleUpdate = async () => {
         loadItems({
             page: page.value,
             itemsPerPage: itemsPerPage.value,
-            sortBy: [{key: 'created_at', order: 'desc'}],
+            sortBy: [{ key: 'created_at', order: 'desc' }],
             search: props.search
         });
         toast.value.message = 'Reader updated successfully!'
@@ -179,7 +179,7 @@ const applyFilters = (data) => {
     loadItems({
         page: page.value,
         itemsPerPage: itemsPerPage.value,
-        sortBy: [{key: 'created_at', order: 'desc'}],
+        sortBy: [{ key: 'created_at', order: 'desc' }],
         search: props.search
     });
 }
@@ -209,129 +209,104 @@ defineExpose({
 </script>
 
 <template>
-    <VDataTableServer
-        v-model:items-per-page="itemsPerPage"
-        :headers="headers"
-        :items="serverItems"
-        :items-length="totalItems"
-        :loading="loading"
-        item-value="id"
-        :search="search"
-        @update:options="loadItems"
-        class="text-no-wrap"
-    >
+    <VDataTableServer v-model:items-per-page="itemsPerPage" :headers="headers" :items="serverItems"
+        :items-length="totalItems" :loading="loading" item-value="id" :search="search" @update:options="loadItems"
+        class="text-no-wrap">
 
-    <template #item.reader_type_id="{ item }">
-        <!-- Use the reader_type_name instead of reader_type_id -->
-        {{ item.reader_type?.name }}
-    </template>
+        <template #item.reader_type_id="{ item }">
+            <!-- Use the reader_type_name instead of reader_type_id -->
+            {{ item.reader_type?.name }}
+        </template>
 
-    <!-- Get plant object and name -->
-    <template #item.plant_code="{ item }">
-        {{ item.plant?.name }}
-    </template>
+        <!-- Get plant object and name -->
+        <template #item.plant_code="{ item }">
+            {{ item.plant?.name }}
+        </template>
 
-    <template #item.last_log="{ item }">
-        <span v-if="item.last_log?.first_seen_timestamp">
-            {{ item.last_log?.first_seen_timestamp ? Moment(item.last_log?.first_seen_timestamp).format('MMMM D, YYYY h:mm A') : '' }}
-        </span>
-    </template>
+        <template #item.last_log="{ item }">
+            <span v-if="item.last_log?.created_at">
+                {{ item.last_log?.created_at ? Moment(item.last_log?.created_at).format('MMMM D, YYYY h:mm A') : '' }}
+            </span>
+        </template>
 
-    <template #item.status="{ item }">
-        <div class="d-flex gap-1 ml-3">
-            <VBadge v-if="item.status == READER_STATUS.ACTIVE" content="ACTIVE" color="success pa-3 px-8"  />
-            <VBadge v-else-if="item.status == READER_STATUS.INACTIVE" content="INACTIVE" color="primary-2 pa-3 px-6 text-grey-100"  />
-            <VBadge v-else content="DEACTIVATED" color="error pa-3"  />
-        </div>
-    </template>
+        <template #item.status="{ item }">
+            <div class="d-flex gap-1 ml-3">
+                <VBadge v-if="item.status == READER_STATUS.ACTIVE" content="ACTIVE" color="success pa-3 px-8" />
+                <VBadge v-else-if="item.status == READER_STATUS.INACTIVE" content="INACTIVE"
+                    color="primary-2 pa-3 px-6 text-grey-100" />
+                <VBadge v-else content="DEACTIVATED" color="error pa-3" />
+            </div>
+        </template>
 
-    <!-- Actions -->
-    <template #item.actions="{ item }">
-        <div class="d-flex gap-1 justify-center">
-            <!-- Picklist  -->
-            <template v-if="item.antennas?.some(antenna => antenna.bay_location)">
-                <v-menu location="start">
-                    <template v-slot:activator="{ props: menuProps }">
-                        <v-tooltip location="top">
-                            <template v-slot:activator="{ props: tooltipProps }">
-                                <v-btn 
-                                    icon="ri-truck-line" 
-                                    variant="text" 
-                                    color="grey" 
-                                    v-bind="{ ...menuProps, ...tooltipProps }"
-                                ></v-btn>
-                            </template>
-                            <span>View Picklist Screen</span>
-                        </v-tooltip>
-                    </template>
-                    <v-list>
-                        <v-list-item
-                            v-for="(antenna, i) in item.antennas.filter(a => a.bay_location)" 
-                            :key="i"
-                            :value="i"
-                        >
-                            <v-list-item-title 
-                                @click="showReaderLastTap(item, antenna.bay_location.bay_no)" 
-                                class="px-4"
-                            >
-                                Bay No. {{ antenna.bay_location.bay_no }}
-                            </v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-            </template>
-            <!-- Loading Curtain -->
-            <template v-if="item.antennas?.some(antenna => antenna.bay_location)">
-                <v-menu location="start">
-                    <template v-slot:activator="{ props: menuProps }">
-                        <v-tooltip location="top">
-                            <template v-slot:activator="{ props: tooltipProps }">
-                                <v-btn 
-                                    icon="ri-computer-line" 
-                                    variant="text" 
-                                    color="grey" 
-                                    v-bind="{ ...menuProps, ...tooltipProps }"
-                                ></v-btn>
-                            </template>
-                            <span>View Loading Curtain Screen</span>
-                        </v-tooltip>
-                    </template>
-                    <v-list>
-                        <v-list-item
-                            v-for="(antenna, i) in item.antennas.filter(a => a.bay_location)" 
-                            :key="i"
-                            :value="i"
-                        >
-                            <v-list-item-title 
-                                @click="showCurtainScreen(item, antenna.bay_location.bay_no)" 
-                                class="px-4"
-                            >
-                                Bay No. {{ antenna.bay_location.bay_no }}
-                            </v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-            </template>
+        <!-- Actions -->
+        <template #item.actions="{ item }">
+            <div class="d-flex gap-1 justify-center">
+                <!-- Picklist  -->
+                <template v-if="item.antennas?.some(antenna => antenna.bay_location)">
+                    <v-menu location="start">
+                        <template v-slot:activator="{ props: menuProps }">
+                            <v-tooltip location="top">
+                                <template v-slot:activator="{ props: tooltipProps }">
+                                    <v-btn icon="ri-truck-line" variant="text" color="grey"
+                                        v-bind="{ ...menuProps, ...tooltipProps }"></v-btn>
+                                </template>
+                                <span>View Picklist Screen</span>
+                            </v-tooltip>
+                        </template>
+                        <v-list>
+                            <v-list-item v-for="(antenna, i) in item.antennas.filter(a => a.bay_location)" :key="i"
+                                :value="i">
+                                <v-list-item-title @click="showReaderLastTap(item, antenna.bay_location.bay_no)"
+                                    class="px-4">
+                                    Bay No. {{ antenna.bay_location.bay_no }}
+                                </v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </template>
+                <!-- Loading Curtain -->
+                <template v-if="item.antennas?.some(antenna => antenna.bay_location)">
+                    <v-menu location="start">
+                        <template v-slot:activator="{ props: menuProps }">
+                            <v-tooltip location="top">
+                                <template v-slot:activator="{ props: tooltipProps }">
+                                    <v-btn icon="ri-computer-line" variant="text" color="grey"
+                                        v-bind="{ ...menuProps, ...tooltipProps }"></v-btn>
+                                </template>
+                                <span>View Loading Curtain Screen</span>
+                            </v-tooltip>
+                        </template>
+                        <v-list>
+                            <v-list-item v-for="(antenna, i) in item.antennas.filter(a => a.bay_location)" :key="i"
+                                :value="i">
+                                <v-list-item-title @click="showCurtainScreen(item, antenna.bay_location.bay_no)"
+                                    class="px-4">
+                                    Bay No. {{ antenna.bay_location.bay_no }}
+                                </v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </template>
 
-            <IconBtn v-if="authStore.user.is_super_admin || authStore.user.is_warehouse_admin"
-                size="small"
-                @click="editItem(item)"
-            >
-            <VIcon icon="ri-pencil-line" />
-            </IconBtn>
-        </div>
-    </template>
+                <IconBtn v-if="authStore.user.is_super_admin || authStore.user.is_warehouse_admin" size="small"
+                    @click="editItem(item)">
+                    <VIcon icon="ri-pencil-line" />
+                </IconBtn>
+            </div>
+        </template>
     </VDataTableServer>
 
     <!-- Not used, but prepared just incase -->
     <DeleteModal @close="deleteDialog = false" :show="deleteDialog" dialog-title="Delete Reader">
         <template #default>
             <VCardText>
-                <p class="text-h5 text-bold-emphasis ps-2">Are you sure you want to delete this reader? This action is irreversible</p>
+                <p class="text-h5 text-bold-emphasis ps-2">Are you sure you want to delete this reader? This action is
+                    irreversible</p>
             </VCardText>
 
             <div class="d-flex justify-end align-center mt-4">
-                <v-btn color="secondary" variant="outlined" @click="deleteDialog = false" class="px-12 mr-3">Cancel</v-btn>
+                <v-btn color="secondary" variant="outlined" @click="deleteDialog = false"
+                    class="px-12 mr-3">Cancel</v-btn>
                 <PrimaryButton @click="handleDelete" color="error" class="px-12" type="submit" :loading="isLoading">
                     Delete
                 </PrimaryButton>
@@ -339,37 +314,28 @@ defineExpose({
         </template>
     </DeleteModal>
 
-    <EditingModal v-if="form.name && form.reader_type_id && form.plant_code" @close="editDialog = false" 
+    <EditingModal v-if="form.name && form.reader_type_id && form.plant_code" @close="editDialog = false"
         :show="editDialog" :dialog-title="`Update ${selectedReader.name}`">
         <template #default>
             <v-form @submit.prevent="handleUpdate">
-                <v-select label="Select Plant" density="compact"
-                    :items="plants" v-model="form.plant_code"
-                    :rules="[value => !!value || 'Please select an item from the list']"
-                >
+                <v-select label="Select Plant" density="compact" :items="plants" v-model="form.plant_code"
+                    :rules="[value => !!value || 'Please select an item from the list']">
                 </v-select>
-                <v-select class="mt-6" label="Select Reader Type" density="compact"
-                    :items="readerTypes" v-model="form.reader_type_id"
-                    :rules="[value => !!value || 'Please select an item from the list']"
-                >
+                <v-select class="mt-6" label="Select Reader Type" density="compact" :items="readerTypes"
+                    v-model="form.reader_type_id" :rules="[value => !!value || 'Please select an item from the list']">
                 </v-select>
-                <v-text-field class="mt-6" density="compact" 
-                    label="Reader Name"
-                    v-model="form.name" 
-                    :rules="[value => !!value || 'Reader name is required']"
-                />
-                <v-text-field class="mt-6" density="compact" 
-                    label="IP Address"
-                    v-model="form.ip_address"
-                />
+                <v-text-field class="mt-6" density="compact" label="Reader Name" v-model="form.name"
+                    :rules="[value => !!value || 'Reader name is required']" />
+                <v-text-field class="mt-6" density="compact" label="IP Address" v-model="form.ip_address" />
             </v-form>
             <div class="d-flex justify-end align-center mt-4">
-                <v-btn color="secondary" variant="outlined" @click="editDialog = false" class="px-12 mr-3">Cancel</v-btn>
+                <v-btn color="secondary" variant="outlined" @click="editDialog = false"
+                    class="px-12 mr-3">Cancel</v-btn>
                 <PrimaryButton @click="handleUpdate" color="primary" class="px-12" type="submit" :loading="isLoading">
                     Update
                 </PrimaryButton>
             </div>
         </template>
     </EditingModal>
-    <Toast :show="toast.show" :message="toast.message" :color="toast.color" @update:show="toast.show = $event"/>
+    <Toast :show="toast.show" :message="toast.message" :color="toast.color" @update:show="toast.show = $event" />
 </template>
