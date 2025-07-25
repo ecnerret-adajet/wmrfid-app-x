91c8dbc7dff8b9aac90ee3d40bb2c4339a6d1f0c
Lorz Becislao <lorenz.becislao@lafilgroup.com>
2025-07-25 18:02:57 +0800

    reader status enum

--- src/composables/useEnums.js
+++ src/composables/useEnums.js
@@ -1,10 +1,11 @@
 export const READER_STATUS = {
-    ACTIVE: 1,
-    INACTIVE: 2,
-    DEACTIVATED: 3
-};
+  ACTIVE: 1,
+  INACTIVE: 2,
+  DEACTIVATED: 3,
+  UNREACHABLE: 4,
+}
 
 export const USER_STATUS = {
-    ACTIVE: 1,
-    INACTIVE: 2,
-};
+  ACTIVE: 1,
+  INACTIVE: 2,
+}
